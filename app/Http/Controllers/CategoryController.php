cd5d7907d87a05f299543536b05b956a42547cb4
AIT <anas.tasadduq@gmail.com>
2019-06-23 10:36:45 +0300

    show all categories on index page

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -15,6 +15,8 @@ class CategoryController extends Controller
     public function index()
     {
         //
+        $categories = Category::all();
+        return view('categories.index', compact('categories'));
     }
 
     /**
