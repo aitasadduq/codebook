00ef9eb88256da9425b26cb58b09926802e65adc
AIT <anas.tasadduq@gmail.com>
2019-06-23 10:36:08 +0300

    update category model and add CRUD routes

--- app/Category.php
+++ app/Category.php
@@ -7,4 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     //
+    public function subCategories ()
+    {
+    	return $this->hasMany(Category::class);
+    }
+
+    public function addSubCategory ($attributes)
+    {
+    	return $this->subCategories()->create($attributes);
+    }
+
+    public function parent ()
+    {
+    	return $this->belongsTo(Category::class, 'parent_id');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('categories', 'CategoryController');
 
 Auth::routes();
 
