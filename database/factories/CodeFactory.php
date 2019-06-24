7dbfa9eb8b06ea5d6de2fc8d1fe6eeca668fa99a
AIT <anas.tasadduq@gmail.com>
2019-06-24 12:13:28 +0300

    create code model, controller, factory and migration (table)

---
+++ database/factories/CodeFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Code;
+use Faker\Generator as Faker;
+
+$factory->define(Code::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/migrations/2019_06_24_085933_create_codes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCodesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('codes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('code_id');
+            $table->text('code');
+            $table->text('details');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('codes');
+    }
+}
