06dcd0f2f24c968ecc67cfd1ceacbc6fded59d5f
AIT <anas.tasadduq@gmail.com>
2019-06-26 13:02:59 +0300

    improve the look of the main page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,12 +4,14 @@
 <br>
 <div class="row">
     @foreach ($categories as $cat)
-    <div class="card col-md-4">
-        <div class="card-body">
-            <h4 class="card-title text-center">{{ $cat->title }}</h4>
-            <p class="card-text">{{ $cat->description }}</p>
-            <div class="text-center">
-                <a class="btn btn-primary" href="/codes">View Codes</a>
+    <div class="col-md-4 col-sm-6 mb-4">
+        <div class="card">
+            <div class="card-body">
+                <h4 class="card-title text-center">{{ $cat->title }}</h4>
+                <p class="card-text">{{ $cat->description }}</p>
+                <div class="text-center">
+                    <a class="btn btn-primary" href="/codes">View Codes</a>
+                </div>
             </div>
         </div>
     </div>
