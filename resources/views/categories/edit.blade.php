baf6b9d9d2db2ac1148e3e707eb232a77fcee39e
AIT <anas.tasadduq@gmail.com>
2019-06-26 13:13:00 +0300

    modify edit category page

--- resources/views/categories/edit.blade.php
+++ resources/views/categories/edit.blade.php
@@ -2,23 +2,27 @@
 @section('content')
 <div class="card">
 	<div class="card-header">
-		<?php $text = ($category->category_id == 0) ? "Category" : "Sub-category" ?>
-		<h1>Edit {{ $text }}</h1>
+		<h1>Edit Category</h1>
 	</div>
 	<div class="card-body">
 		<form method="POST" action="/categories/{{ $category->id }}">
 			@csrf
 			@method('PATCH')
-			<input type="hidden" name="category_id" value="{{ $category->category_id }}">
-			<input type="text" name="title" class="form-control" placeholder="Title" value="{{ $category->title }}">
-			<br>
-			<input class="btn btn-primary" type="submit" name="submit" value="Edit {{ $text }}">
+			<div class="form-group">
+				<label for="title">Title</label>
+				<input type="text" id="title" name="title" class="form-control" value="{{ $category->title }}">
+			</div>
+			<div class="form-group">
+				<label for="description">Description</label>
+				<textarea id="description" name="description" class="form-control">{{ $category->description }}</textarea>
+			</div>
+			<input class="btn btn-primary" type="submit" name="submit" value="Edit Category">
 		</form>
 		<br>
 		<form method="POST" action="/categories/{{ $category->id }}">
 			@csrf
 			@method('DELETE')
-			<input class="btn btn-danger" type="submit" name="submit" value="Delete {{ $text }}">
+			<input class="btn btn-danger" type="submit" name="submit" value="Delete Category">
 		</form>
 	</div>
 </div>
