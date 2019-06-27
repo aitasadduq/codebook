372801e941fe02e62f36c7f85a989c7e43abe74a
AIT <anas.tasadduq@gmail.com>
2019-06-27 11:49:43 +0300

    improve the appearance of code viewing page

--- resources/views/codes/index.blade.php
+++ resources/views/codes/index.blade.php
@@ -1,23 +1,43 @@
 @extends('layouts.app')
 @section('content')
-<div class="text-center"><h1>View Codes</h1></div>
+<div class="text-center"><h1>View {{ $category->title }} Codes</h1></div>
 <br>
 <div class="row">
 	<div class="col-md-3">
-		<ul class="list-group">
-			@foreach($category->subCategories as $sub)
-			<li class="list-group-item d-flex justify-content-between align-items-center">
-				{{ $sub->title }}
-			</li>
-			@endforeach
-		</ul>
+		<form method="POST" action="/categories/{{ $category->id }}/codes">
+			<div class="card">
+				<div class="card-body text-center">
+					<h3 class="card-title">Subcategories</h3>
+				</div>
+				<ul class="list-group list-group-flush">
+					@foreach($category->subCategories as $sub)
+					<li class="list-group-item">
+						<div class="form-check">
+							<input class="form-check-input" type="checkbox" name="{{ $sub->id }}" id="{{ $sub->id }}">
+							<label class="form-check-label" for="{{ $sub->id }}">{{ $sub->title }}</label>
+						</div>
+					</li>
+					@endforeach
+				</ul>
+				<div class="card-body text-center">
+					<input class="btn btn-primary" type="submit" name="submit" value="Filter">
+				</div>
+			</div>
+		</form>
 	</div>
 	<div class="col-md-9">
 		<ul class="list-group">
-			@foreach($codes as $code)
+			@foreach($category->codes as $code)
 				@if($code->code_id == 0)
 					<li class="list-group-item d-flex justify-content-between align-items-center">
 						<a href="/codes/{{ $code->id }}">{{ $code->title }}</a>
+						<div>
+							@foreach($code->subcategories as $sub)
+							<span class="badge badge-primary badge-pill">
+								{{ $sub->title }}
+							</span>
+							@endforeach
+						</div>
 					</li>
 				@endif
 			@endforeach
