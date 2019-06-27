3d442ed5601583e94cdb4a2b028588aff6667614
AIT <anas.tasadduq@gmail.com>
2019-06-27 11:13:20 +0300

    fix intermediate table migration (table) naming

--- database/migrations/2019_06_27_080926_create_code_subcategory_table.php
+++ database/migrations/2019_06_27_080926_create_code_subcategory_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateSubcategoryCodeTable extends Migration
+class CreateCodeSubcategoryTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateSubcategoryCodeTable extends Migration
      */
     public function up()
     {
-        Schema::create('subcategory_code', function (Blueprint $table) {
+        Schema::create('code_subcategory', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->unsignedInteger('subcategory_id');
             $table->unsignedInteger('code_id');
@@ -28,6 +28,6 @@ class CreateSubcategoryCodeTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('subcategory_code');
+        Schema::dropIfExists('code_subcategory');
     }
 }
