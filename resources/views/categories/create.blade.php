eb2e80e49f81af58e5053573b96f18177ca2c4f5
AIT <anas.tasadduq@gmail.com>
2019-06-27 09:49:25 +0300

    center align category and subcategory create and edit pages

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -1,22 +1,28 @@
 @extends('layouts.app')
 @section('content')
-<div class="card">
-	<div class="card-body">
-		<h1 class="card-title">Add New Category</h1>
-		<form method="POST" action="/categories">
-			@csrf
-			<div class="form-group">
-				<label for="title">Title</label>
-				<input type="text" id="title" name="title" class="form-control">
-			</div>
-			<div class="form-group">
-				<label for="description">Description</label>
-				<textarea id="description" name="description" class="form-control"></textarea>
-			</div>
-			<input class="btn btn-primary" type="submit" name="submit" value="Add Category">
-		</form>
+<div class="row">
+	<div class="col-md-2"></div>
+	<div class="card col-md-8">
+		<div class="card-body text-center">
+			<h1 class="card-title">Add New Category</h1>
+			<form method="POST" action="/categories">
+				@csrf
+				<div class="form-group text-left">
+					<label for="title">Title</label>
+					<input type="text" id="title" name="title" class="form-control">
+				</div>
+				<div class="form-group text-left">
+					<label for="description">Description</label>
+					<textarea id="description" name="description" class="form-control"></textarea>
+				</div>
+				<input class="btn btn-primary" type="submit" name="submit" value="Add Category">
+			</form>
+		</div>
 	</div>
+	<div class="col-md-2"></div>
 </div>
 <br>
-<a class="btn btn-primary" href="/categories">View All Categories</a>
+<div class="text-center">
+	<a class="btn btn-primary" href="/categories">View All Categories</a>
+</div>
 @endsection
--- resources/views/subcategories/edit.blade.php
+++ resources/views/subcategories/edit.blade.php
@@ -1,17 +1,21 @@
 @extends('layouts.app')
 @section('content')
-<div class="card">
-	<div class="card-body">
-		<h1 class="card-title">Edit Subcategory</h1>
-		<form method="POST" action="/categories/{{ $category->id }}/subcategories/{{ $subcategory->id }}">
-			@csrf
-			@method('PATCH')
-			<div class="form-group">
-				<label for="title" >Title</label>
-				<input class="form-control" type="text" name="title" id="title">
-			</div>
-			<input class="btn btn-primary" type="submit" name="submit" value="Edit Subcategory">
-		</form>
+<div class="row">
+	<div class="col-md-2"></div>
+	<div class="card text-center col-md-8">
+		<div class="card-body">
+			<h1 class="card-title">Edit Subcategory</h1>
+			<form method="POST" action="/categories/{{ $category->id }}/subcategories/{{ $subcategory->id }}">
+				@csrf
+				@method('PATCH')
+				<div class="form-group text-left">
+					<label for="title" >Title</label>
+					<input class="form-control" type="text" name="title" id="title">
+				</div>
+				<input class="btn btn-primary" type="submit" name="submit" value="Edit Subcategory">
+			</form>
+		</div>
 	</div>
+	<div class="col-md-2"></div>
 </div>
 @endsection
